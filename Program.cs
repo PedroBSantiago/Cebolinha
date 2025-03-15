5d665ca393c9b73ab67bfc5b660f893120421e54
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 22:37:57 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string cebola;
+Console.Clear();
+Console.Write("Escreva sua frase bacana e top :");
+cebola = Console.ReadLine()!
+.Replace("r","l")
+.Replace("R","L");
+
+Console.WriteLine($"Cebolinha responde : {cebola}");
+
